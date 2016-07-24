d985ab45f975bbeec28fb0bfad091aeaf606cf60
ravali <ravali.888@gmail.com>
2016-07-24 21:01:15 +0530

    online auction system

--- online_auction_system/settings.py
+++ online_auction_system/settings.py
@@ -83,20 +83,21 @@ WSGI_APPLICATION = 'online_auction_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'online_auction_system',
-        'USER': 'root',
-        'PASSWORD': 'leanest#',
-        'HOST': 'localhost',
-        'OPTIONS': {
-            'autocommit': True,
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'online_auction_system',
+#         'USER': 'root',
+#         'PASSWORD': 'leanest#',
+#         'HOST': 'localhost',
+#         'OPTIONS': {
+#             'AUTOCOMMIT': True,
+#         }
+#     }
+# }
 
 db_from_env = dj_database_url.config()
+DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 
