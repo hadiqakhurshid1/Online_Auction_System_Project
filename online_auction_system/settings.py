88b50567972073d452a77d5147c111dad3eb3200
ravali <ravali.888@gmail.com>
2016-07-27 09:56:06 +0530

    online auction system

--- online_auction_system/settings.py
+++ online_auction_system/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'online_auction_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-#
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
